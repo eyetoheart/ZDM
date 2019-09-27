a835581cb0eac0717f86a83ef958b34dfe1a66b9
eyetoheart <32696860+eyetoheart@users.noreply.github.com>
2019-09-27 10:06:47 +0800

    Update dellog.sh

--- dellog.sh
+++ dellog.sh
@@ -3,7 +3,6 @@
 SCRIPTDIR="/home/iwgroup/backup/script/dellog"
 CONFIGFILE="$SCRIPTDIR/config.txt"
 
-echo "delete 5 days ago file,`date -d '-0 day' "+%Y-%m-%d_%H:%M:%S"`" >> $SCRIPTDIR/logs.log
 for i in `cat $CONFIGFILE`
 do
     delNdayago="`echo $i | awk -F, '{print $1}'`"
@@ -11,11 +10,12 @@ do
 
     if [ -d $delpath ]
     then
- 
+        echo "delete directory $delpath  $delNdayago days ago file,`date -d '-0 day' "+%Y-%m-%d_%H:%M:%S"`" >> $SCRIPTDIR/logs.log 
         cd $delpath
         find ./ -name "*20*.log"  -mtime "+$delNdayago" | xargs rm -rf {} \;
     fi
 done
+
 [iwgroup@DEMO2 dellog]$ cat config.txt 
 5,/opt/iwgroup/vasoss4.3/elsm3.1/fat/scheduleProcessor/log/all
 5,/opt/iwgroup/vasoss4.3/elsm3.1/fat/scheduleProcessor/log/business
