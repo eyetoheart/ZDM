f3603716019a0486726dd7fea7418ef077fe8132
eyetoheart <32696860+eyetoheart@users.noreply.github.com>
2019-10-12 19:00:19 +0800

    Update check_up_winfile.sh

--- check_up_winfile.sh
+++ check_up_winfile.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 SCRIPTDIR="/home/postgres/backup/script/check_up_winfile"
-YESTERDAY="`date -d '-0 day' "+%Y%m%d"`"
+YESTERDAY="`date -d '-0 day' "+%Y-%m-%d"`"
 source ~/.bash_profile
+
+FLAGDATE="81900"
+FILEDATE="82800"
+CURRENT_HOUR="`date +"%H"`"
+CURRENT_MINUTE="`date +"%M"`"
+CURRENT_SECOND="`date +"%S"`"
+CURRENT_SECOND="`expr $CURRENT_HOUR \* 3600 + $CURRENT_MINUTE \* 60 + $CURRENT_SECOND`"
 
 DBSERVER="192.168.108.27"
 DBSERVERPORT="5432"
 DBSERVERUSER="postgres"
 DBSERVERDBNAME="lottomagic"
-
+DBSERVERPATH="winprize"
 
 ftpfile () {
 echo "open $FTPSERVER $FTPPORT" > $SCRIPTDIR/ftptask.txt
@@ -16,7 +23,7 @@ echo "user $FTPUSER $FTPPASS" >> $SCRIPTDIR/ftptask.txt
 echo "verbose" >> $SCRIPTDIR/ftptask.txt
 echo "$FTPMODE" >> $SCRIPTDIR/ftptask.txt
 echo "binary" >> $SCRIPTDIR/ftptask.txt
-echo "cd $FTPPATH" >> $SCRIPTDIR/ftptask.txt
+echo "cd $DBSERVERPATH" >> $SCRIPTDIR/ftptask.txt
 echo "ls $LOCALFILE" >> $SCRIPTDIR/ftptask.txt
 echo "close" >> $SCRIPTDIR/ftptask.txt
 echo "bye" >> $SCRIPTDIR/ftptask.txt
@@ -38,7 +45,7 @@ echo "-------------------`date -d '-0 day' "+%Y-%m-%d %H:%M:%S"`----------------
 
 if [ -s $SCRIPTDIR/issue.txt ]
 then
-    echo "win_prize file OK" >> $SCRIPTDIR/logs.txt
+    echo "winPrize issue file OK" >> $SCRIPTDIR/logs.txt
 
     for  i  in  `cat $SCRIPTDIR/config.txt`
     do
@@ -49,34 +56,57 @@ then
         FTPPORT="`echo $i | awk -F, '{print $4}'`"
         FTPMODE="`echo $i | awk -F, '{print $5}'`"
         FTPPASS="`echo $i | awk -F, '{print $6}'`"
-        FTPPATH="winprize"
+        FTPALIASES="`echo $i | awk -F, '{print $7}'`"
     
         LOCALFILEPATH="/home/postgres/backup/script/data_from_108.25nfs/winPrizeFile/file/$FTPNAME"
+        LOCALFLAGPATH="/home/postgres/backup/script/data_from_108.25nfs/winPrizeFile/flag/$FTPNAME"
         for win in `cat $SCRIPTDIR/issue.txt`
         do
             gamename="`echo $win | awk -F, '{print $1}'`"
             gameissue="`echo $win | awk -F, '{print $2}'`"
             LOCALFILE="win_prize_${gamename}_${gameissue}.txt"
-            if [ -f $LOCALFILEPATH/$LOCALFILE ]
+            LOCALFLAG="win_prize_${gamename}_${gameissue}_flag.txt"
+
+            if [[ $CURRENT_SECOND -ge $FLAGDATE && $CURRENT_SECOND -lt $FILEDATE ]]
             then
-                LOCALFILESIZE="`ls -l $LOCALFILEPATH/$LOCALFILE | awk  '{print $5}'`"
-                ftpfile
-                grep $LOCALFILE  $SCRIPTDIR/tempfile.log  >> $SCRIPTDIR/logs.txt 2>&1
-                if [ $? -ne 0 ]
+                if [ -s $LOCALFLAGPATH/$LOCALFLAG ]
                 then
-                    mailcontent="今天没有将本地的$LOCALFILE文件上传给商户$FTPNAME的FTP服务器,请检查"
-                    sendmailtouser $mailcontent
-                else
-                    RFILESIZE="`grep $LOCALFILE $SCRIPTDIR/tempfile.log | awk '{print $5}'`"
-                    if [ $LOCALFILESIZE -ne $RFILESIZE ]
+                    flagcontent="`cat $LOCALFLAGPATH/$LOCALFLAG`"
+                    if [ $flagcontent != "ok" ]
                     then
-                        mailcontent="上传给商户$FTPNAME的中奖文件与本地文件大小不一致，请检查"
+                        mailcontent="$FTPNAME-$FTPALIASES,中奖标志文件生成有问题,内容不是ok"
                         sendmailtouser $mailcontent
                     fi
+                else
+                    mailcontent="$FTPNAME-$FTPALIASES,中奖标志文没有生成,或者生成有问题,内容为空"
+                    sendmailtouser $mailcontent
                 fi
-            else
-                mailcontent="今天本地服务器没有生成商户$FTPNAME的中奖文件，请检查"
-                sendmailtouser $mailcontent
+                if [ ! -f $LOCALFILEPATH/$LOCALFILE ]
+                then
+                    mailcontent="$FTPNAME-$FTPALIASES,中奖文件没有生成"
+                    sendmailtouser $mailcontent
+                fi
+            fi
+            if [ $CURRENT_SECOND -ge $FILEDATE ]
+            then
+                if [ -f $LOCALFILEPATH/$LOCALFILE ]
+                then
+                    LOCALFILESIZE="`ls -l $LOCALFILEPATH/$LOCALFILE | awk  '{print $5}'`"
+                    ftpfile
+                    grep $LOCALFILE  $SCRIPTDIR/tempfile.log  >> $SCRIPTDIR/logs.txt 2>&1
+                    if [ $? -ne 0 ]
+                    then
+                        mailcontent="今天没有将本地的$LOCALFILE文件上传给商户$FTPNAME-$FTPALIASES的FTP服务器,请检查"
+                        sendmailtouser $mailcontent
+                    else
+                        RFILESIZE="`grep $LOCALFILE $SCRIPTDIR/tempfile.log | awk '{print $5}'`"
+                        if [ $LOCALFILESIZE -ne $RFILESIZE ]
+                        then
+                            mailcontent="上传给商户$FTPNAME-$FTPALIASES的中奖文件与本地文件大小不一致，请检查"
+                            sendmailtouser $mailcontent
+                        fi
+                    fi
+                fi
             fi
         done
     done
@@ -89,9 +119,27 @@ rm -rf $SCRIPTDIR/issue.txt
 
 
 
-该脚本的配置文件的名字是：config.txt，内容如下：
-商户代码,FTP服务器,FTP登录账号,FTP端口,模式,FTP密码
-C10000,116.228.224.195,ftpuser,21,passive,ftppasswd
-C21000,47.95.197.220,ftpuser,21,passive,ftppasswd
-C20000,47.98.112.62,ftpuser,52121,,ftppasswd
-c23000,47.100.6.77,ftpuser,21,passive,ftppasswd
+
+
+
+
+
+
+
+
+
+config.txt配置文件内容：
+C10000,116.228.224.195,gctrpiwtftp,21,passive,ftppassword,guocaitong
+C21000,47.95.197.220,yingtai,21,passive,ftppassword,huijinkeji
+c23000,47.100.6.77,iwgroup,21,passive,ftppassword,zhuowangxinxi
+C25000,47.95.197.220,yingtaisv2,21,passive,ftppassword,huijinkeji2
+
+
+
+
+
+receiver.txt文件内容：
+rmt@iwgroup.com.cn,zhangdaoming
+yangf@iwgroup.com.cn,yangfan
+13552961152@139.com,yangfan
+13661089847@139.com,zdm
