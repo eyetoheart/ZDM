48b1b8aceddf92f868089fd23645361cc0dcac86
eyetoheart <32696860+eyetoheart@users.noreply.github.com>
2019-10-12 18:41:21 +0800

    Update check_upfile_state.sh

--- check_upfile_state.sh
+++ check_upfile_state.sh
@@ -2,7 +2,15 @@
 
 SCRIPTDIR="/home/iwgroup/backup/script/check_upfile_state"
 YESTERDAY="`date -d '-1 day' "+%Y%m%d"`"
+FLAGDATE="6600"
+FILEDATE="7800"
+CURRENT_HOUR="`date +"%H"`"
+CURRENT_MINUTE="`date +"%M"`"
+CURRENT_SECOND="`date +"%S"`"
+CURRENT_SECOND="`expr $CURRENT_HOUR \* 3600 + $CURRENT_MINUTE \* 60 + $CURRENT_SECOND`"
 LOCALFILE="trans-data-${YESTERDAY}.txt"
+LOCALFLAG="flag-${YESTERDAY}.txt"
+source ~/.bash_profile
 
 ftpfile () {
 echo "open $FTPSERVER $FTPPORT" > $SCRIPTDIR/ftptask.txt
@@ -22,7 +30,7 @@ sendmailtouser () {
 for user in `awk -F, '{print $1}' $SCRIPTDIR/receiver.txt`
 do
     echo "now start send mail to $user,time is `date +"%H:%M:%S"`" >> $SCRIPTDIR/logs.txt
-    echo $1 | mutt -s "彩运无限系统对账文件生成或上传有故障:$YESTERDAY" $user
+    echo $1 | mutt -s "彩运无限系统对账文件或标志文件生成或上传有故障:$YESTERDAY" $user
 done
 }
 
@@ -37,30 +45,54 @@ do
     FTPPORT="`echo $i | awk -F, '{print $4}'`"
     FTPMODE="`echo $i | awk -F, '{print $5}'`"
     FTPPASS="`echo $i | awk -F, '{print $6}'`"
+    FTPALIASES="`echo $i | awk -F, '{print $7}'`"
     FTPPATH="transdata"
     
+    LOCALFLAGPATH="/home/iwgroup/backup/script/data_from_108.25nfs/channelOrderSettleFile/flag/$FTPNAME"
     LOCALFILEPATH="/home/iwgroup/backup/script/data_from_108.25nfs/channelOrderSettleFile/settleFile/$FTPNAME"
 
-    if [ -f $LOCALFILEPATH/$LOCALFILE ]
+    if [[ $CURRENT_SECOND -ge $FLAGDATE && $CURRENT_SECOND -lt $FILEDATE ]]
     then
-        LOCALFILESIZE="`ls -l $LOCALFILEPATH/$LOCALFILE | awk  '{print $5}'`"
-        ftpfile
-        grep $LOCALFILE  $SCRIPTDIR/tempfile.log  >> $SCRIPTDIR/logs.txt 2>&1
-        if [ $? -ne 0 ]
+        if [ -s $LOCALFLAGPATH/$LOCALFLAG ]
         then
-            mailcontent="昨天没有将本地的$LOCALFILE文件上传给商户$FTPNAME的FTP服务器,请检查"
-            sendmailtouser $mailcontent
-        else
-            RFILESIZE="`grep $LOCALFILE $SCRIPTDIR/tempfile.log | awk '{print $5}'`"
-            if [ $LOCALFILESIZE -ne $RFILESIZE ]
+            flagcontent="`cat $LOCALFLAGPATH/$LOCALFLAG`"
+            if [ $flagcontent != "ok" ]
             then
-                mailcontent="上传给商户$FTPNAME的对账文件与本地文件大小不一致，请检查"
+                mailcontent="$FTPNAME-$FTPALIASES,对账标志文件生成有问题,内容不是ok"
                 sendmailtouser $mailcontent
             fi
+        else
+            mailcontent="$FTPNAME-$FTPALIASES,对账标志文件没有生成,或者生成有问题,内容为空"
+            sendmailtouser $mailcontent
         fi
-    else
-        mailcontent="昨天本地服务器没有生成商户$FTPNAME的对账文件，请检查"
-        sendmailtouser $mailcontent
+
+        if [ ! -f $LOCALFILEPATH/$LOCALFILE ]
+        then
+            mailcontent="昨天本地服务器没有生成商户$FTPNAME-$FTPALIASES的对账文件，请检查"
+            sendmailtouser $mailcontent
+        fi
+    fi
+    if [ $CURRENT_SECOND -gt $FILEDATE ]
+    then
+
+        if [ -f $LOCALFILEPATH/$LOCALFILE ]
+        then
+            LOCALFILESIZE="`ls -l $LOCALFILEPATH/$LOCALFILE | awk  '{print $5}'`"
+            ftpfile
+            grep $LOCALFILE  $SCRIPTDIR/tempfile.log  >> $SCRIPTDIR/logs.txt 2>&1
+            if [ $? -ne 0 ]
+            then
+                mailcontent="昨天没有将本地的$LOCALFILE文件上传给商户$FTPNAME-$FTPALIASES的FTP服务器,请检查"
+                sendmailtouser $mailcontent
+            else
+                RFILESIZE="`grep $LOCALFILE $SCRIPTDIR/tempfile.log | awk '{print $5}'`"
+                if [ $LOCALFILESIZE -ne $RFILESIZE ]
+                then
+                    mailcontent="上传给商户$FTPNAME-$FTPALIASES的对账文件与本地文件大小不一致，请检查"
+                    sendmailtouser $mailcontent
+                fi
+            fi
+        fi
     fi
 done
 
@@ -71,9 +103,21 @@ rm -rf $SCRIPTDIR/tempfile.log
 
 
 
-该脚本的配置文件的名字是：config.txt，内容如下：
-商户代码,FTP服务器,FTP登录账号,FTP端口,模式,FTP密码
-C10000,116.228.224.195,ftpuser,21,passive,ftppasswd
-C21000,47.95.197.220,ftpuser,21,passive,ftppasswd
-C20000,47.98.112.62,ftpuser,52121,,ftppasswd
-c23000,47.100.6.77,ftpuser,21,passive,ftppasswd
+
+
+
+
+
+
+该脚本文件的配置文件：
+C10000,116.228.224.195,gctrpiwtftp,21,passive,ftppassword,guocaitong
+C21000,47.95.197.220,yingtai,21,passive,ftppassword,huijinkeji
+c23000,47.100.6.77,iwgroup,21,passive,ftppassword,zhuowangxinxi
+C25000,47.95.197.220,yingtaisv2,21,passive,ftppassword,huijinkeji2
+
+
+receiver.txt文件内容：
+rmt@iwgroup.com.cn,yunweibu
+yangf@iwgroup.com.cn,yangfan
+13552961152@139.com,yangfan
+13661089847@139.com,zhangdm
