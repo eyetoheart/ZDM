c888f866f99a2cbf9dff9f202317c9d3d78577d5
eyetoheart <32696860+eyetoheart@users.noreply.github.com>
2021-01-22 15:32:50 +0800

    Update restartapp.sh

--- restartapp.sh
+++ restartapp.sh
@@ -82,3 +82,15 @@ do
     done
     wait
 done
+
+
+
+
+脚本文件同目录下config.txt为脚本的配置文件，该配置文件以":"分割3列，第一列为程序目录，即tomcat部署位置；第二列是数据目录，即部署jar包的位置，第三列为排除目录，此目录下即便有文件 更新也不重新应用，第三列可有可无，非必须列，以下是例子：
+/opt/tomcat-1:/data/test_dir_1:/data/test_dir_1/manager
+/opt/tomcat-2:/data/test_dir_2
+脚本监控第二列所在目录下文件是否有更新，如果有更新则等待N分钟，然后重启应用，等待期间如再有文件更新，则再等待N分钟，直到N分钟之内无文件更新时再重启应用。
+
+配置文件config.txt的内容如下：
+/opt/tomcat-1:/data/test_dir_1:/data/test_dir_1/manager
+/opt/tomcat-2:/data/test_dir_2 
